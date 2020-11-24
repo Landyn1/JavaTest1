175397ebc7582bb9191e5ea876a5889a2bf1b19b
lyh <572453930@qq.com>
2020-11-24 12:25:27 +0800

    test1

---
+++ src/test/java/testTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testTest {
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void run() {
+        assertEquals("是时间",new test().run("12:00:00"));
+    }
+}
